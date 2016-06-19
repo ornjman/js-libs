f9a89f9482a7b64702b440707ae6eee7ea53f774
Kyle <ornjman@gmail.com>
2016-06-19 00:26:25 -0400

    adding function to get index of element within the parent

--- es-extension.js
+++ es-extension.js
@@ -17,3 +17,23 @@ function reload_back_forward_cache(){
 }
 //
 //
+// ------------------------------
+// Get Element Position in Parent
+// ------------------------------
+// about: 	get index (integer) of child position within the parent element
+function getIndex(node) {
+	// capture children of element's parent
+	var children = node.parentNode.childNodes;
+	// iterate through the children of the element's parent
+	for (i = 0; i < children.length; i++) {
+		// check if the node in question matches the element in the array
+		if (node == children[i]){
+			// exit the loop
+			break;
+		}
+	}
+	// return the positional index (integer)
+	return i;
+}
+//
+//
