f81c9d424a9450bc63bd689611fb92aaa1f8dd05
ornjman <ornjman@gmail.com>
2017-02-07 09:37:08 -0500

    adding extensions to math library

---
+++ math-extension.js
@@ -0,0 +1,10 @@
+
+// Note:    This function was not designed by me but included from the Mozilla Developer Page
+// Source:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// End sourced function from Mozilla Developer Page
+
