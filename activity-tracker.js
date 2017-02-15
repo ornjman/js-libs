992f5049e981ed2a002f0e3a2c1eb1c3cb854132
ornjman <ornjman@gmail.com>
2017-02-15 14:39:47 -0500

    adding activity tracker for determining status of users

---
+++ activity-tracker.js
@@ -0,0 +1,65 @@
+//
+// Name:        Activity Tracker
+// Author:      ornjman
+// About:       This class helps to track activity for testing whether or not 
+//              the user is active or idle.
+//              
+var activityTracker = function () {
+
+  // timeout variable
+  var t;
+
+  // minimum time that has to pass before being considered idle
+  var minimumIdleTime = 10000; 
+
+  // callback storage arrays
+  var idleCallbacks = [];
+  var activeCallbacks = [];
+
+  window.onload = resetTimer;
+  
+  document.onmousemove = resetTimer;
+  document.onkeypress = resetTimer;
+
+  // functions to register active/idle functions that are to be called
+  function registerActiveCallback(functionName){
+    activeCallbacks.push(functionName);
+  }
+  function registerIdleCallback(functionName){
+    idleCallbacks.push(functionName);
+  }
+
+  // called when the minimumIdleTime is exceeded to call all registered functions
+  function idle(){
+    for(var i = 0; i < idleCallbacks.length; i++){
+      idleCallbacks[i]();
+    }
+  }
+
+  // called when activity occurs after going idle to call all registered functions
+  // note that any blocking functions will have issues if the time to call those
+  // functions exceed the minimumIdleTime
+  function active(){
+    for(var i = 0; i < activeCallbacks.length; i++){
+      activeCallbacks[i]();
+    }
+  }
+  // called when activity occurs to reset the timer
+  function resetTimer() {
+    clearTimeout(t);
+    t = setTimeout(idle, minimumIdleTime);
+    active();
+  }
+  // function to set the idle time value
+  function setIdleTime(minimumTime){
+    minimumIdleTime = minimumTime;
+  }
+
+  var o = new Object();
+
+  // register functions for scope
+  o.registerActiveCallback = registerActiveCallback;
+  o.registerIdleCallback = registerIdleCallback;
+
+  return o;
+};
